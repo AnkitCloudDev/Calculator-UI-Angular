04c8c48c978d9a57ea7ad5900d38e711dd8bbcd2
ANKIT DONGRE <ankitdongre96@gmail.com>
2025-03-19 21:24:37 +0530

    fixed investment result component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,25 @@ import { HeaderComponent } from './component/header/header.component';
 import { UserInputComponent } from './component/user-input/user-input.component';
 import { InvestmentResultComponent } from './component/investment-result/investment-result.component';
 import { InvestmentData } from './model/investment-data.model';
+import { InvestmentCalculation } from './service/investment-calculation.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent,UserInputComponent,InvestmentResultComponent]
+  imports: [HeaderComponent,UserInputComponent,InvestmentResultComponent],
+  providers: [InvestmentCalculation]
 })
 export class AppComponent {
     investmentData: InvestmentData= new InvestmentData();
+    investmentResults: any[] = [];
 
-    submit: boolean= false;
+    constructor(private investmentCalculationService: InvestmentCalculation){}
 
     onSubmit(data: InvestmentData){
         this.investmentData = data;
-        this.submit = true;
+        this.investmentResults = this.investmentCalculationService.calculateInvestmentResults(this.investmentData);
     }
+
+
 }
--- src/app/component/investment-result/investment-result.component.ts
+++ src/app/component/investment-result/investment-result.component.ts
@@ -1,22 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { InvestmentCalculation } from '../../service/investment-calculation.service';
 import { InvestmentData } from '../../model/investment-data.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css',
-  providers: [InvestmentCalculation]
 })
 export class InvestmentResultComponent {
-  @Input() investmentData!: InvestmentData;
+
   @Input() investmentResults: any[] = [];
   constructor(private investmentCalculationService: InvestmentCalculation) {}
 
 
-  calculateInvestmentResults() {
-    this.investmentResults = this.investmentCalculationService.calculateInvestmentResults(this.investmentData);
-}
+
   }
