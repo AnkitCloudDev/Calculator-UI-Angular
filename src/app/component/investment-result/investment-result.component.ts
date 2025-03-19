3e6b13c8d0ac0e62d3ea3df4fbb8f3284f78b0fa
ANKIT DONGRE <ankitdongre96@gmail.com>
2025-03-19 21:42:33 +0530

    fixed table display

--- src/app/component/investment-result/investment-result.component.ts
+++ src/app/component/investment-result/investment-result.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { InvestmentCalculation } from '../../service/investment-calculation.service';
-import { InvestmentData } from '../../model/investment-data.model';
-import { NgFor } from '@angular/common';
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, CurrencyPipe],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css',
 })
